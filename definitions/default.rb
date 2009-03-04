7ebfb66fe18e7d38a1abea44f743908d3ed58f7d
Joshua Sierles <joshua@rasgueado.local>
2009-03-04 21:56:07 +0100

    move files around and add sample configs plus instructions on boostrapping chef-server

--- definitions/default.rb
+++
@@ -1,7 +0,0 @@
-define :add_htpasswd_users do
-  params[:users].each do |user, pass|
-    execute "htpasswd add #{user}" do
-      command "/usr/bin/htpasswd -b #{params[:name]} #{user} #{pass}"
-    end
-  end
-end
--- definitions/htpasswd.rb
+++
@@ -1,8 +0,0 @@
-define :htpasswd_file, :entries => {"default" => "04j9j02kv"} do
-  template params[:name] do
-    owner "www-data"
-    mode 0750
-    source "htpasswd.erb"
-    variables(:entries => params[:entries])
-  end
-end
