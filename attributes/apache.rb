c2a7bc39fb2f13cac132cc6db621597fb825e9c9
Joshua Sierles <jsierles@gmail.com>
2010-04-06 14:52:29 +0200

    major updates for 0.8 compatibility. some recipes now use databags by default.

--- attributes/apache.rb
+++ attributes/apache.rb
@@ -1,53 +1,39 @@
-apache Mash.new unless attribute?("apache")
+default.apache[:dir]     = "/etc/apache2" 
+default.apache[:log_dir] = "/var/log/apache2"
+default.apache[:user]    = "www-data"
+default.apache[:binary]  = "/usr/sbin/apache2"
+default.apache[:icondir] = "/usr/share/apache2/icons"
+default.apache[:version] = "2.2.9-7ubuntu3.5"
 
-# Where the various parts of apache are
-apache[:dir]     = "/etc/apache2" 
-apache[:log_dir] = "/var/log/apache2"
-apache[:user]    = "www-data"
-apache[:binary]  = "/usr/sbin/apache2"
-apache[:icondir] = "/usr/share/apache2/icons"
+default.apache[:listen_ports] = ["80"]     
+default.apache[:contact] = "sysadmins@37signals.com" 
+default.apache[:timeout] = 300
+default.apache[:keepalive] = "Off"
+default.apache[:keepaliverequests] = 12
+default.apache[:keepalivetimeout] = 2
 
-apache[:version] = "2.2.9-7ubuntu3.5"
-###
-# These settings need the unless, since we want them to be tunable,
-# and we don't want to override the tunings.
-###
+default.apache[:prefork] = Mash.new 
+default.apache[:prefork][:startservers] = 16
+default.apache[:prefork][:minspareservers] = 16
+default.apache[:prefork][:maxspareservers] = 32
+default.apache[:prefork][:maxclients] = 256
+default.apache[:prefork][:maxrequestsperchild] = 10000
 
-# General settings
-apache[:listen_ports] = [ "80" ]     unless apache.has_key?(:listen_ports)
-apache[:contact] = "sysadmins@37signals.com" unless apache.has_key?(:contact)
-apache[:timeout] = 300               unless apache.has_key?(:timeout)
-apache[:keepalive] = "Off"            unless apache.has_key?(:keepalive)
-apache[:keepaliverequests] = 12     unless apache.has_key?(:keepaliverequests)
-apache[:keepalivetimeout] = 2        unless apache.has_key?(:keepalivetimeout)
+default.apache[:worker] = Mash.new
+default.apache[:worker][:serverlimit] = 16
+default.apache[:worker][:startservers] = 4
+default.apache[:worker][:maxclients] = 1024
+default.apache[:worker][:minsparethreads] = 64
+default.apache[:worker][:maxsparethreads] = 192
+default.apache[:worker][:threadsperchild] = 64
+default.apache[:worker][:maxrequestsperchild] = 0
 
-# Prefork Attributes
-apache[:prefork] = Mash.new unless apache.has_key?(:prefork)
-apache[:prefork][:startservers] = 16      unless apache[:prefork].has_key?(:prefork_startservers)
-apache[:prefork][:minspareservers] = 16   unless apache[:prefork].has_key?(:prefork_minspareservers)
-apache[:prefork][:maxspareservers] = 32   unless apache[:prefork].has_key?(:prefork_maxspareservers)
-apache[:prefork][:maxclients] = 256       unless apache[:prefork].has_key?(:prefork_maxclients)
-apache[:prefork][:maxrequestsperchild] = 10000 unless apache[:prefork].has_key?(:prefork_maxrequestsperchild)
+default.apache[:deflate][:mime_types] = %w(text/html text/plain text/xml application/xml application/xhtml+xml
+                                           text/javascript application/x-javascript application/javascript text/css)
+default.apache[:deflate][:disable] = false
 
-# Worker Attributes
-apache[:worker] = Mash.new unless apache.has_key?(:worker)
-apache[:worker][:serverlimit] = 16        unless apache[:worker].has_key?(:serverlimit)
-apache[:worker][:startservers] = 4        unless apache[:worker].has_key?(:startservers)
-apache[:worker][:maxclients] = 1024       unless apache[:worker].has_key?(:maxclients)
-apache[:worker][:minsparethreads] = 64    unless apache[:worker].has_key?(:minsparethreads)
-apache[:worker][:maxsparethreads] = 192   unless apache[:worker].has_key?(:maxsparethreads)
-apache[:worker][:threadsperchild] = 64    unless apache[:worker].has_key?(:threadsperchild)
-apache[:worker][:maxrequestsperchild] = 0 unless apache[:worker].has_key?(:maxrequestsperchild)
-
-# Module configuration
-apache[:deflate] = Mash.new unless apache.has_key?(:deflate)
-apache[:deflate][:mime_types] = %w(text/html text/plain text/xml application/xml application/xhtml+xml
-                                text/javascript application/x-javascript application/javascript text/css) unless apache[:deflate].has_key?(:mime_types)
-apache[:deflate][:disable] = false unless apache[:deflate].has_key?(:disable)
-
-apache[:expires] = Mash.new unless apache.has_key?(:expires)
-apache[:expires][:default] = "access plus 1 year" unless apache[:expires].has_key?(:default)                                
-apache[:expires][:match] = "\.(ico|gif|jpe?g|png|js|css)$" unless apache[:expires].has_key?(:match)
-apache[:expires][:set] = {"Cache-Control" => "public"} unless apache[:expires].has_key?(:set)
-apache[:expires][:unset] = %w(Last-Modified ETag) unless apache[:expires].has_key?(:unset)
-apache[:expires][:file_etag] = "None" unless apache[:expires].has_key?(:file_etag)
+default.apache[:expires][:default] = "access plus 1 year"
+default.apache[:expires][:match] = "\.(ico|gif|jpe?g|png|js|css)$"
+default.apache[:expires][:set] = {"Cache-Control" => "public"}
+default.apache[:expires][:unset] = %w(Last-Modified ETag)
+default.apache[:expires][:file_etag] = "None"
